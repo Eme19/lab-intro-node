824a1bff9357ba280c9cd6395e9a00dd945d474c
“Emeka <ngwe7236@gmail.com>
2023-05-18 03:37:54 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,85 @@
-class SortedList {
-  constructor() {}
 
-  add(item) {}
+class  SortedList {
+  constructor(items,length) {
+    this.items=[];
+    this.length=0;
 
-  get(pos) {}
+  }
 
-  max() {}
 
-  min() {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+   return this.items.sort(function(x,y) {
+   
+   if(x < y){
+     return -1;
+   }
+   if(x > y){
+     return 1;
+   }
+   return 0;
+  });
 
-  sum() {}
+ }
 
-  avg() {}
+  get(pos) {
+   this.items ;
+   this.length++
+  
+    if(pos >= this.items.length){
+      throw new Error('OutOfBounds');
+     
+    }
+      return  this.items[pos];
+    
+  }
+
+  max() {
+
+    // this.items.reduce((a,b) => {
+      if( this.length === 0){
+        throw new Error ("EmptySortedList")
+      }else{
+        return this.items[this.length -1] ;
+      }
+      
+   // })
+  }
+
+  min() {
+   if (this.length === 0){
+    throw new Error ("EmptyStoredList")
+   }
+   else{
+      return this.items[0];
+   }
+  }
+
+  sum() {
+  return this.items.reduce((a,b) => a + b, 0)
+  }
+
+
+  avg() {
+    // this.items.reduce((total, amount) => {
+    //   if(total.length == amount){
+    //     throw new Error ("EmptySortedList")
+    //   }else{
+    //     return total / amount ;
+    //   }
+    this.sum()
+    if(this.length === 0){
+      throw new Error ("EmptyStoredList");
+    }
+    else{
+      return this.sum () / this.length;
+    }
+
+  }
+      
+   // })
 }
+
 
 module.exports = SortedList;
